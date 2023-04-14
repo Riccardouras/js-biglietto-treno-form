43ce0b662a7fd1ab569ce5962dca85829deb069b
[Riccardo Uras] <[riccardo.uras3@gmail.com]>
2023-04-14 18:24:17 +0200

    non riesco a capire che ho sbagliato

--- js/main.js
+++ js/main.js
@@ -5,22 +5,32 @@ console.log(`km`);
 let eta = document.getElementById("inputGroupSelect01");
 console.log(`eta`);
 let nomeCognome = document.getElementById("nm")
+console.log(`nomeCognome`)
 
 function generaBiglietto () {
     let prezzo = 0.21 * km;
     if (eta="1"){
-        prezzo *= 0.8;
+        prezzo *= 0.8.toFixed(2);
+        document.getElementById("Biglietto").innerHTML = `Sconto Minori`;
+        document.getElementById("costo").innerHTML = `${prezzo}`;
+        document.getElementById("Nome").innerHTML = `${nomeCognome}`;
     } else if (eta="2"){
-        prezzo *= 0.6;
-    } 
+        prezzo *= 0.6.toFixed(2);
+        document.getElementById("Biglietto").innerHTML = `Sconto Anziani`;
+        document.getElementById("costo").innerHTML = `${prezzo}`;
+        document.getElementById("Nome").innerHTML = `${nomeCognome}`;
+    } else{
+        document.getElementById("Nome").innerHTML = `${nomeCognome}`;
+        document.getElementById("costo").innerHTML = `${prezzo}`;
+    }
     console.log(`prezzo`);
 }
 function annullaBiglietto (){
-    document.getElementById("km").innerHTML = ``;
+    document.getElementById("km").innerHTML = `Tratta`;
     km= null;
-    document.getElementById("inputGroupSelect01").innerHTML = ``;
+    // document.getElementById("inputGroupSelect01").innerHTML = ``;
     eta= null;
-    document.getElementById("nm").innerHTML = ``;
+    document.getElementById("nm").innerHTML = `Inserisci nome`;
     nomeCognome= null;
 }
 
